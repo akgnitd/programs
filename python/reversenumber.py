53f3667a69d2e5c31236c9d3e3e9f8ad6fcda121
akgnit <akgnit7@gmail.com>
2018-06-22 00:43:14 +0530

    reversenumber

---
+++ python/reversenumber.py
@@ -0,0 +1,10 @@
+def reversenum(number):
+      result = 0
+      while number>0:
+          remainder = number % 10
+          result = result *10 + remainder
+          number = number/10
+          print number,result
+      print "Reverse: ",result
+
+reversenum(int(raw_input("Enter number: ")))
--- python/reversestring.md
+++ python/reversestring.md
@@ -1,2 +1,3 @@
 reverse1() - reverse a string.
+
 reverse2() - reverse each word in a string without changing its index.
