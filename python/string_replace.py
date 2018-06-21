efddfb015f62632716e220c4ee16eed672815af1
akgnit <akgnit7@gmail.com>
2018-06-22 02:42:35 +0530

    str replace

---
+++ python/string_replace.py
@@ -0,0 +1,4 @@
+def str_replace(string,old_sub_string,new_sub_string):
+    print "New String: ",string.replace(old_sub_string,new_sub_string)
+
+str_replace(raw_input('Enter String: '),raw_input('Substring to be replaced: '),raw_input('New Substring: '))
